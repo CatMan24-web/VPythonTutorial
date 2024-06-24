533283a1ff35db10dd6f3040f25a9dc6a88f27e4
CatMan24-web <ryanchen02262011@gmail.com>
2024-06-24 11:12:50 +0100

    Update 3_1DMotion.py

--- 03.等速度直線運動/3_1DMotion.py
+++ 03.等速度直線運動/3_1DMotion.py
@@ -12,7 +12,7 @@ from vpython import *
 size = 0.05   # side length of wood block
 l = 1        # floor length
 v = 0.03     # Block speed
-t = 0        # time
+t = 4        # time
 dt = 0.01    # Time interval
 
 """
@@ -20,8 +20,8 @@ dt = 0.01    # Time interval
 
 """
 scene = canvas(title="1D Motion", width=800, height=600, x=0, y=0, center=vec(0, 0.1, 0), background=vec(0, 0.6, 0.6))
-floor = box(pos=vec(0, 0, 0), size=vec(L, 0.1*size, 0.5*L), color=color.red)
-cube = box(pos=vec(-0.5*L + 0.5*size, 0.55*size, 0), size=vec(size, size, size), color=color.blue, v=vec(v, 0, 0))
+floor = box(pos=vec(0, 0, 0), size=vec(L, 0.1*size, 0.5*L), color=color.blue)
+cube = box(pos=vec(-0.5*L + 0.5*size, 0.55*size, 0), size=vec(size, size, size), color=color.red, v=vec(v, 0, 0))
 gd = graph(title="x-t plot", width=600, height=450, x=0, y=600, xtitle="t(s)", ytitle="x(m)")
 gd2 = graph(title="v-t plot", width=600, height=450, x=0, y=1050, xtitle="t(s)", ytitle="v(m/s)")
 xt = gcurve(graph=gd, color=color.red)
